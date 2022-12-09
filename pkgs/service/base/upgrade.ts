0114ec481fdbf4cb07bc9e81592f2f02d7b45122
Rizky <rizky05@gmail.com>
2022-12-09 23:49:22 +0700

    upgrade 6

--- pkgs/service/base/upgrade.ts
+++ pkgs/service/base/upgrade.ts
@@ -70,6 +70,7 @@ export const baseUpgrade = async () => {
   console.log(`Updating pkgs`);
   await removeAsync(join(process.cwd(), "pkgs"));
   await moveAsync(join(tempdir, "pkgs"), join(process.cwd(), "pkgs"));
+  process.stdout.write(` ▒`);
   await runPnpm(["i"], process.cwd());
 
   const root = process.cwd();
@@ -81,7 +82,7 @@ export const baseUpgrade = async () => {
 
         if (await existsAsync(join(root, "app", f, "upgrade.ts"))) {
           console.log(`\n › Upgrading ${f}`);
-          process.stdout.write(`    ▒`);
+          process.stdout.write(` ▒`);
           if (
             !await existsAsync(join(root, "app", f, "node_modules", "jiti"))
           ) {
