86560a446cf1729461eb55ec05bf05b313347c62
Rizky <rizky05@gmail.com>
2023-03-25 10:22:33 +0700

    fix mantap

--- app/app.ts
+++ app/app.ts
@@ -12,6 +12,4 @@ initialize(async () => {
   await service.db._process.start();
   await service.srv._process.start();
   await service.web._process.start();
-
-  await service.coba._process.start();
 });
